f2846572be42c513d6306823f54b0f3627676b54
Aaron Signer <aaronsigner1995@proton.me>
2025-10-20 10:19:12 -0400

    Added empty home and shows comp

---
+++ src/app/shows/shows.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mm-shows',
+  imports: [],
+  templateUrl: './shows.html',
+  styleUrl: './shows.css'
+})
+export class Shows {
+
+}

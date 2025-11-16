9fb26b4a61c90ddb3c4c63dcffd8cd84ac949750
Aaron Signer <aaronsigner1995@proton.me>
2025-11-15 19:50:40 -0500

    Changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Shows } from './shows/shows';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home, title: 'Home'},
+    { path: 'shows', component: Shows, title: 'Shows'}
+];

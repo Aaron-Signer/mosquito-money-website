46d9a263d5e1ea5de9802d4bfd86cb735512a568
Aaron Signer <aaronsigner1995@proton.me>
2026-04-21 21:59:16 -0400

    Migration

--- src/app/event/ievent.ts
+++ src/app/event/ievent.ts
@@ -1,2 +1,5 @@
 export interface IEvent {
+    location: String; 
+    date: Date; 
+    venue: String;
 }
--- src/app/shows/shows.ts
+++ src/app/shows/shows.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './shows.css'
 })
 export class Shows {
-
+	
 }
